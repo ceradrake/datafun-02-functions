c290e1c7010287cd590c7988355bb41b6bd79fcf
Cera Drake <cera.drake1@gmail.com>
2023-09-02 20:11:29 -0500

    Task4

---
+++ user_math_ceradrake.py
@@ -0,0 +1,26 @@
+""" Cera Drake 9/2/2023 This module will represent math in the floral industry"""
+import math
+
+from util_logger import setup_logger
+logger, logname = setup_logger(__file__)
+
+
+# calculate the area of a floral arrangement packing box #
+
+get_area_of_packingbox(heighth,width)
+
+
+area = get_area_of_packingbox(heighth,width)
+
+
+logger.info("Area of a floral packing box")
+logger.info(f"math.comb(4,4) = {math.comb(4,4)}")
+
+
+
+
+
+
+logger.info("Explore some functions in the math module")
+logger.info(f"math.comb(5,1) = {math.comb(5,1)}")
+logger.info(f"math.perm(5,1) = {math.perm(5,1)}")
