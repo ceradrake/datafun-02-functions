1d63e44f3342bebd399217b01667ea6c79019d56
Cera Drake <cera.drake1@gmail.com>
2023-09-02 21:28:57 -0500

    Create 3 objects total and log

--- oop_ceradrake.py
+++ oop_ceradrake.py
@@ -20,7 +20,23 @@ if __name__ == "__main__":
     data1 = "10"
     loc1 = "South America"
 
-    object1 = NumericFloralSeries(name1, units1, data1, loc1)
+    name2 = "Gypsophilia"
+    units2 = "Bunch"
+    data2 = "20"
+    loc2 = "South America"
 
+    name3 = "Gerbera Daisy"
+    units3 = "Stem"
+    data3 = "40"
+    loc3 = "Denmark"
+
+    object1 = NumericFloralSeries(name1, units1, data1, loc1)
+    object2 = NumericFloralSeries(name1, units2, data2, loc2)
+    object3 = NumericFloralSeries(name3, units3, data3, loc3)
+
+
+logger.info(f"Created: {object1}")
+logger.info(f"Created: {object2}")
+logger.info(f"Created: {object3}")
 
 
